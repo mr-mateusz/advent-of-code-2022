a792f442e88a2e029f761e5691c84eb056bb4448
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2023-06-03 21:50:38 +0200

    day17 - baseline (not finished)

--- day_17/solution.py
+++ day_17/solution.py
@@ -4,7 +4,10 @@ import itertools
 from dataclasses import dataclass
 from typing import List
 
+import numpy as np
 
+
+# Todo - make coord immutable
 @dataclass
 class Coord:
     x: int
@@ -36,13 +39,22 @@ class Rock:
         return cls(generate_rock_coords(left, bottom, rock_type))
 
     def can_move_left(self, chamber: Chamber) -> bool:
-        pass
+        for coord in self.coords:
+            if not chamber.is_coord_available(Coord(coord.x - 1, coord.y)):
+                return False
+        return True
 
     def can_move_right(self, chamber: Chamber) -> bool:
-        pass
+        for coord in self.coords:
+            if not chamber.is_coord_available(Coord(coord.x + 1, coord.y)):
+                return False
+        return True
 
     def can_move_down(self, chamber: Chamber) -> bool:
-        pass
+        for coord in self.coords:
+            if not chamber.is_coord_available(Coord(coord.x, coord.y - 1)):
+                return False
+        return True
 
     def __move(self, x: int, y: int) -> None:
         for coord in self.coords:
@@ -66,15 +78,78 @@ class Chamber:
         self.width = width
         self.bottom = bottom
 
+        # todo change Coord from dataclass to namedtuple and change the list below to set
         self.coords_taken = []
 
         self.rock_type_iter = itertools.cycle(range(rock_types_count))
         self.jet_iter = itertools.cycle(jet_pattern)
 
+    def is_coord_available(self, coord: Coord) -> bool:
+        if coord in self.coords_taken:
+            return False
+        if coord.x < 0:
+            return False
+        if coord.x > self.width:
+            return False
+        if coord.y <= self.bottom:
+            return False
+        return True
+
+    @property
+    def tower_height(self) -> int:
+        return max([coord.y for coord in self.coords_taken], default=self.bottom)
+
+    def simulate(self, num_rocks: int) -> None:
+        # todo
+        for _ in range(num_rocks):
+            rock_type = next(self.rock_type_iter)
+            rock = Rock.create(2, self.tower_height + 4, rock_type)
+            rock_landed = False
+
+            while not rock_landed:
+                self.print()
+                # Push by jet
+                jet_direction = next(self.jet_iter)
+
+                if jet_direction == '<':  # todo - enum
+                    if rock.can_move_left(self):
+                        rock.move_left()
+                elif jet_direction == '>':
+                    if rock.can_move_right(self):
+                        rock.move_right()
+
+                # Fall down
+                if rock.can_move_down(self):
+                    rock.move_down()
+                else:
+                    rock_landed = True
+                    self.coords_taken.extend(rock.coords)
+
+    def print(self):
+        # fixme
+        arr = np.ones((self.tower_height + 1, self.width))
+        for coord in self.coords_taken:
+            arr[coord.y, coord.x] = 0
+
+        arr = np.flip(arr, 0)[:-1, :]
+        print(arr)
+
+
 
 if __name__ == '__main__':
     path = './input2.txt'
-    path = 'year_2022/day_17/input2.txt'
+
+    rocks_to_fall = 2
 
     with open(path, 'r', encoding='utf-8') as f:
         jet_pattern = f.read().strip()
+
+    chamber = Chamber(7, 0, len(rock_types), jet_pattern)
+
+    chamber.simulate(rocks_to_fall)
+
+    print(max([coord.y for coord in chamber.coords_taken]))
+
+    print(chamber.coords_taken)
+
+    chamber.print()
