cc569142f2b1ac1658196f02a2bcfff21969d6f6
mr-mateusz <37747987+mr-mateusz@users.noreply.github.com>
2023-09-24 10:49:12 +0200

    day 18 part 2

--- day_18/solution.py
+++ day_18/solution.py
@@ -15,7 +15,7 @@ class Coord(NamedTuple):
         return f'({self.x}, {self.y}, {self.z})'
 
 
-def adjacent_positions(position: tuple[int, int, int]):
+def adjacent_positions(position: Coord):
     neighbor_positions_relative = [Coord(-1, 0, 0),
                                    Coord(1, 0, 0),
                                    Coord(0, -1, 0),
@@ -25,6 +25,13 @@ def adjacent_positions(position: tuple[int, int, int]):
 
     for rel_pos in neighbor_positions_relative:
         yield position + rel_pos
+
+
+def is_adjacent_to_water(position: Coord, water_positions: set[Coord]) -> bool:
+    for adjacent_position in adjacent_positions(position):
+        if adjacent_position in water_positions:
+            return True
+    return False
 
 
 def calculate_surface(lava_bits_positions: set[Coord]) -> int:
@@ -49,4 +56,60 @@ if __name__ == '__main__':
     lava_bits_positions = set(data)
 
     # part 1
+    print(calculate_surface(lava_bits_positions))
+
+    # part 2 - simulation
+
+    # min and max value for each coord ('edges' of the 3D space)
+    coord_mins = [min(pos[i] for pos in lava_bits_positions) for i in range(3)]
+    coord_maxs = [max(pos[i] for pos in lava_bits_positions) for i in range(3)]
+
+    # initial water positions (just over the 'edges')
+    water_positions = set(
+        [Coord(coord_mins[0] - 1, y, z)
+         for y in range(coord_mins[1], coord_maxs[1] + 1)
+         for z in range(coord_mins[2], coord_maxs[2] + 1)] +
+        [Coord(coord_maxs[0] + 1, y, z)
+         for y in range(coord_mins[1], coord_maxs[1] + 1)
+         for z in range(coord_mins[2], coord_maxs[2] + 1)] +
+        [Coord(x, coord_mins[1] - 1, z)
+         for x in range(coord_mins[0], coord_maxs[0] + 1)
+         for z in range(coord_mins[2], coord_maxs[2] + 1)] +
+        [Coord(x, coord_maxs[1] + 1, z)
+         for x in range(coord_mins[0], coord_maxs[0] + 1)
+         for z in range(coord_mins[2], coord_maxs[2] + 1)] +
+        [Coord(x, y, coord_mins[2] - 1)
+         for x in range(coord_mins[0], coord_maxs[0] + 1)
+         for y in range(coord_mins[1], coord_maxs[1] + 1)] +
+        [Coord(x, y, coord_maxs[2] + 1)
+         for x in range(coord_mins[0], coord_maxs[0] + 1)
+         for y in range(coord_mins[1], coord_maxs[1] + 1)]
+    )
+
+    # empty space positions (initially positions in 'space' without lava bits)
+    empty_space_positions = set([Coord(x, y, z)
+                                 for x in range(coord_mins[0], coord_maxs[0] + 1)
+                                 for y in range(coord_mins[1], coord_maxs[1] + 1)
+                                 for z in range(coord_mins[2], coord_maxs[2] + 1)
+                                 if Coord(x, y, z) not in lava_bits_positions])
+
+    # simulate water flooding the space
+    water_expanded = True
+    while water_expanded:
+        water_expanded = False
+        water_will_reach_positions = []
+        for empty_space_position in empty_space_positions:
+            if is_adjacent_to_water(empty_space_position, water_positions):
+                water_will_reach_positions.append(empty_space_position)
+
+        if water_will_reach_positions:
+            water_expanded = True
+            water_positions.update(water_will_reach_positions)
+            empty_space_positions = empty_space_positions.difference(water_will_reach_positions)
+
+    # now treat all 'not flooded' positions as lava positions
+    lava_bits_positions.update(empty_space_positions)
+
+    # and calculate surface for 'updated' lava positions
+    # part 2
     print(calculate_surface(lava_bits_positions))
